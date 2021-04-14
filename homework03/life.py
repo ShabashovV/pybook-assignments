4c10a6a27643e11fd2be7672e3aa8c7306b570a7
ShabashovV <71067418+ShabashovV@users.noreply.github.com>
2021-04-14 15:28:12 +0300

    Update life.py

--- homework03/life.py
+++ homework03/life.py
@@ -11,12 +11,8 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self,
-        size: tp.Tuple[int, int],
-        randomize: bool = True,
-        max_generations: tp.Optional[float] = float("inf"),
-    ) -> None:
+
+    def __init__(self, size: tp.Tuple[int, int], randomize: bool = True, max_generations: tp.Optional[float] = float("inf"),    ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
         # Предыдущее поколение клеток
@@ -29,21 +25,59 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        grid = []
+        for i in range(self.rows):
+            line = []
+
+            for j in range(self.cols):
+                if randomize:
+                    box = random.randint(0, 1)
+                else:
+                    box = 0
+
+                line.append(box)
+            grid.append(line)
+
+
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        list_all_neighbours = []
+        for i in range(cell[0] - 1, cell[0] + 2):
+            if i >= self.rows or i < 0:
+                continue
+            for j in range(cell[1] - 1, cell[1] + 2):
+                if j >= self.cols or j < 0:
+                    continue
+                if i == cell[0] and j == cell[1]:
+                    continue
+                list_all_neighbours.append(self.curr_generation[i][j])
+        return list_all_neighbours
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        next_grid = self.create_grid(False)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.curr_generation[i][j] == 1:
+                    if 2 <= sum(self.get_neighbours((i, j))) <= 3:
+                        next_grid[i][j] = 1
+                    else:
+                        next_grid[i][j] = 0
+                else:
+                    if sum(self.get_neighbours((i, j))) == 3:
+                        next_grid[i][j] = 1
+                    else:
+                        next_grid[i][j] = 0
+
+        return next_grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
+        self.prev_generation = self.curr_generation
+        self.curr_generation = self.get_next_generation()
+        self.generations += 1
         pass
 
     @property
@@ -51,6 +85,7 @@ class GameOfLife:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
+        return self.generations >= self.max_generations
         pass
 
     @property
@@ -58,17 +93,45 @@ class GameOfLife:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        rows = len(self.prev_generation)
+        for i in range(rows):
+            cols = len(self.prev_generation[i])
+            for j in range (cols):
+                if self.curr_generation[i][j] != self.prev_generation[i][j]:
+                    return True
+        return False
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
+        with open(filename, 'r') as file:
+            ob = json.load(file)
+
+        game = GameOfLife(
+            size=(ob['rows'], ob['cols']),
+            randomize=False,
+            max_generations=ob['max_generations']
+        )
+
+        game.prev_generation = ob['prev_generation']
+        game.curr_generation = ob['curr_generation']
+        game.generations = ob['generations']
+        return game
         pass
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        json_obj = {
+            'rows': self.rows,
+            'cols': self.cols,
+            'prev_generation': self.prev_generation,
+            'curr_generation': self.curr_generation,
+            'max_generations': self.max_generations,
+            'generations': self.generations
+        }
+        with open(filename, 'w') as file:
+            json.dump(json_obj, file)
